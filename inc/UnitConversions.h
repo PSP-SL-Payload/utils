0ec6eb6861bddfd6a595a99aabd727ff7c753bc8
seanboltjes <sboltjes@purdue.edu>
2022-06-01 18:36:16 -0500

    Basic layout

---
+++ inc/UnitConversions.h
@@ -0,0 +1,19 @@
+#ifndef UNIT_CONVERSIONS_H
+#define UNIT_CONVERSIONS_H
+
+
+
+class Conversions
+{
+public:
+    static float CelsiusToFahrenheit(float celsius);
+    static float FahrenheitToCelsius(float fahrenheit);
+    static float MetersToFeet(float meters);
+    static float FeetToMeters(float feet);
+
+private:
+
+};
+
+
+ #endif
